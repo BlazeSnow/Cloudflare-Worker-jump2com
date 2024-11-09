4a78f0d6719a9e872525cc82cbfa078fd84adfab
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2024-11-09 23:54:00 +0800

    加入跳转com功能

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+addEventListener('fetch', event => {
+	event.respondWith(handleRequest(event.request));
+});
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+async function handleRequest(request) {
+	const url = new URL(request.url);
+
+	url.hostname = 'blazesnow.com';
+
+	return Response.redirect(url.toString(), 302);
+}
